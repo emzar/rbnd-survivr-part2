a0b550a6d439846d2223f3343cb2d291380deb81
Ivan Mironov <ivan.mironov@gmail.com>
2016-05-06 00:27:04 +0300

    passing test_tribe_to_s and test_tribal_council_immune_is_not_voted_off

--- lib/tribe.rb
+++ lib/tribe.rb
@@ -3,5 +3,17 @@ class Tribe
 
   def initialize(options = {})
     puts options[:name]
+    @name = options[:name]
+    @members = options[:members]
+  end
+
+  def to_s
+    @name
+  end
+
+  def tribal_council(immune)
+    victims = members
+    victims.delete(immune)
+    victims.shuffle(random: Random.new(members.length)).first
   end
 end
